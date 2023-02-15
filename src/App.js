e84d75316d69cd52980adbbb74ba8619504fa4b7
kimseungmin1 <min0107212@naver.com>
2023-02-15 21:57:02 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <Hello />
-      <Hello />
-      <Hello />
-      <Welcome />
+      <div className="box">App</div>
     </div>
   );
 }
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,11 +1,20 @@
 import World from "./World";
+import "./Hello.css";
 
 export default function Hello() {
     return (
       <div>
-        <h1>Hello</h1>
-        <World />
-        <World />
+        <h1
+          style={{
+            color: "#f00",
+            borderRight: "2px solid #000",
+            marginBottom: "50px",
+            opacity: 1,
+          }}
+        >
+          Hello
+        </h1>
+        <div className="box">Hello</div>
       </div>
     );
 }
